c83e5c3dc1ed069225536cf1013e8b31ca2c35ef
Gijs Koot <gijs@spotr.ai>
2021-12-01 16:59:28 +0100

    add comments

--- day1/solve.jl
+++ day1/solve.jl
@@ -1,9 +1,12 @@
+# read input
 x = open("input") do f
     parse.(Int, readlines(f))
 end
 
+# part A
 println(sum(x[2:end] .> x[1:end-1]))
 
+# part B
 sums = x[1:end-2] + x[2:end-1] + x[3:end]
 inc = sums[2:end] .> sums[1:end-1]
 
